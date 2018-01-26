bad9e5a9f9ca215e548487b94bf8817fa7810f5b
maynard <maynardblack@hotmail.com>
2018-01-26 03:04:16 +0000

    Trying to fix POST - have to fix the gem first

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Kenna API Client
-gem 'kenna', '~> 0.1.1'
-gem 'rest-client', '~> 2.0', '>= 2.0.2'
-gem 'faker', '~> 1.8', '>= 1.8.7'
+#gem 'kenna', '~> 0.1.1'
+#gem 'rest-client', '~> 2.0', '>= 2.0.2'
+#gem 'faker', '~> 1.8', '>= 1.8.7'
+#gem 'kenna', :git => 'https://github.com/maynard/kenna.git'
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,29 @@
 Rails.application.routes.draw do
   get 'endpoints/vulnerabilities'
-
   get 'endpoints/assets'
-
   get 'endpoints/asset_groups'
-
   get 'endpoints/asset_tagging'
-
   get 'endpoints/fixes'
-
   get 'endpoints/connectors'
-
   get 'endpoints/users'
-
   get 'endpoints/roles'
-
   get 'endpoints/clients'
-
   get 'endpoints/data_exports'
-
   get 'about/introduction'
   get 'about/authentication'
   get 'about/parameters'
   get 'about/data_types'
   get 'about/errors'
   get 'about/status_codes'
-  
   get 'signout/index'
-  
   get 'pages/introduction'
   get 'pages/wrappers'
   get 'pages/using_tool'
   get 'pages/key'
+  
   post 'pages/key'
-
+  post 'endpoints/users'
+  
   devise_for :users
   get 'welcome/index'
   
@@ -42,3 +32,4 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
